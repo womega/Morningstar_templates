82a1f7f8da0338201b62ace998253bfce833c193
w_omega <38392322+womega@users.noreply.github.com>
2024-08-28 16:02:06 -0400

    Update submit_mnist_job.sh

--- slurm_scripts/submit_mnist_job.sh
+++ slurm_scripts/submit_mnist_job.sh
@@ -1,7 +1,6 @@
-
 #!/bin/bash
 #SBATCH --account=research
-#SBATCH --gpus=1
+#SBATCH --gpus=ampere:1
 #SBATCH --time=02:00:00
 #SBATCH -o $HOME/Morningstar_templates/slurm_scripts/out/mnist_output.out
 #SBATCH -e $HOME/Morningstar_templates/slurm_scripts/err/mnist_error.err
